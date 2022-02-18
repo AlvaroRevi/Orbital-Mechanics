96ea0ac19570d44f85b7f2711d2ff20ab3a6a717
AlvaroRevi <99970937+AlvaroRevi@users.noreply.github.com>
2022-02-18 16:18:59 +0100

    Actualizacion de las funciones principales

--- Session 1/True2Ecc.m
+++ Session 1/True2Ecc.m
@@ -1,4 +1,13 @@
 function E = True2Ecc(theta, e)
+% Obtain the eccentric anomaly of a elliptic orbit with true anomaly 
+% and e as inputs. 
+%
+% Inputs: 
+%    theta: true anomaly 
+%    e: eccentricity 
+%
+% Output: 
+%    E: eccentric anomaly (rads)
 
     y = ((1-e.^2).^(1/2).*sin(theta))./(1+e.*cos(theta)); %sin
     x = (e+cos(theta))./(1+e.*cos(theta)); %cos
