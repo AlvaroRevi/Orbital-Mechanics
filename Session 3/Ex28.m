7e9bc2a749dfdeb1fbd4cff77a59d9c19a9c65bb
AlvaroRevi <99970937+AlvaroRevi@users.noreply.github.com>
2022-02-24 20:50:34 +0100

    Update Ex28.m

--- Session 3/Ex28.m
+++ Session 3/Ex28.m
@@ -1,6 +1,7 @@
 % Exercise 28 
 clear; clc; close all 
 muE = 398600;
+
 % Position vector in ECI basis for the three observations 
 r1 = [-2014.52,4254.09,5409];
 r2 = [-9550.16,-1683.95,9097.54]; 
@@ -14,10 +15,12 @@ D = cross(r2,r3) + cross(r2,r1) + cross(r1,r2) ;
 N = norm(r1)*(cross(r2,r3))+norm(r2)*cross(r3,r1)+norm(r3)*cross(r1,r2); 
 S = (norm(r2)-norm(r3))*r1 + (norm(r3)-norm(r1))*r2 + (norm(r1) - norm(r2))*r3; 
 
+% Compute velocity vector using the Gibbs formula 
 v2 = (1/norm(r2))*sqrt(muE/(norm(N)*norm(D)))*cross(D,r2) + sqrt(muE/(norm(N)*norm(D)))*S; 
 
-[a,e,RAAN,i,omega,theta] = rv2coe(r2,v2)
-
+% Get the Classical Orbit Elements starting from position and velocity
+% vectors
+[a,e,RAAN,i,omega,theta] = rv2coe(r2,v2);
 RAAN = rad2deg(RAAN); 
 i = rad2deg(i);
 omega = rad2deg(omega);
