e623c986f2abcb745a35245e8e4395c6ef2e566a
henrysentana <100405470@alumnos.uc3m.es>
2022-04-18 11:00:28 +0200

    Avances homework 2

---
+++ Homework2/RK4.m
@@ -0,0 +1,16 @@
+function X = RK4(X,dt,flag)
+
+    tf = 90*24*3600;
+    t = 0:dt:tf;
+
+    n = length(t)-1;
+
+    for i = 1:n
+        k1 = Cowell(X(:,i),flag);
+        k2 = Cowell(X(:,i)+dt*k1/2,flag);
+        k3 = Cowell(X(:,i)+dt*k2/2,flag);
+        k4 = Cowell(X(:,i)+k3,flag);
+        X(:,i+1) = X(:,i)+((k1+2*k2+2*k3+k4)/6)*dt;
+    end
+
+end
