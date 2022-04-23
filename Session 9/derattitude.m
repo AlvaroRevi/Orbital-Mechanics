449958b63381da0abefc6612c4abb4d4dbc0f1c5
henrysentana <100405470@alumnos.uc3m.es>
2022-04-24 01:13:00 +0200

    Cambios session 9

---
+++ Session 9/derattitude.m
@@ -0,0 +1,17 @@
+function dydt = derattitude(t,y,Ix,Iy,Iz)
+    
+    psi = y(1);
+    theta = y(2);
+    phi = y(3);
+    omegax = y(4);
+    omegay = y(5);
+    omegaz = y(6);
+
+    dydt(1,1) = (omegax*sin(phi)+omegay*cos(phi))/sin(theta);
+    dydt(2,1) = omegax*cos(phi)-omegay*sin(phi);
+    dydt(3,1) = omegaz - dangles_dt(1,1)*cos(theta);
+    dydt(4,1) = (Iy-Iz)*omegay*omegaz/Ix;
+    dydt(5,1) = (Iz-Ix)*omegax*omegaz/Iy;
+    dydt(6,1) = (Ix-Iy)*omegax*omegay/Iz;
+
+end
